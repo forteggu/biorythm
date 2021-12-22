536e13e6c70303bd9e9e5a82ed47f8037cef6f3f
Fran <fog1.trabajo@gmail.com>
2021-12-22 16:05:37 +0300

    Reviewed navigation routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,27 +8,32 @@ import { MainComponent } from './Components/main/main.component';
 import { NotFoundComponent } from './Components/not-found/not-found.component';
 
 const routes: Routes = [
-  {path:'main', component:MainComponent, children:[
-    {
-      path:"chartView",
-      component:ChartViewComponent
-    },
-    {
-      path:"factorsEdit",
-      component:FactorsEditComponent
-    },
-    {
-      path:"addValues",
-      component:AddValuesComponent
-    }
-  ]},
-  {path:'', component:ChartViewComponent},
-  {path:'**',component:NotFoundComponent}
+  {
+    path: 'main',
+    component: MainComponent,
+    children: [
+      { path: '', redirectTo: '/main/chartView', pathMatch: 'full' },
+      {
+        path: 'chartView',
+        component: ChartViewComponent,
+      },
+      {
+        path: 'factorsEdit',
+        component: FactorsEditComponent,
+      },
+      {
+        path: 'addValues',
+        component: AddValuesComponent,
+      },
+    ],
+  },
+  { path: '', redirectTo: '/main/chartView', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
-export const routesModules = [LoginComponent,MainComponent];
+export class AppRoutingModule {}
+export const routesModules = [LoginComponent, MainComponent];
